352b02476dd23a1569f34e6c00c1246bd152f3d5
Sandeep Kandula <sandeep.kandula@thermofisher.com>
2022-02-22 15:53:19 +0530

    Return API resources.

---
+++ ReturnAPI/UserInfo/UserInfo.API/Controllers/PointsOfInterestController.cs
@@ -0,0 +1,49 @@
+﻿using System.Runtime.InteropServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UserInfo.API.Models;
+
+namespace UserInfo.API.Controllers
+{
+    [Route("api/users/{userId}/pointsofinterest")]
+    [ApiController]
+    public class PointsOfInterestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<PointOfInterestDto>> GetPointsOfInterest(int userId)
+        {
+            var user = UsersDataStore.Current.Users.FirstOrDefault(u => u.Id == userId);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user.PointsOfInterest);
+        }
+
+        [HttpGet("{pointofinterestid}")]
+        public ActionResult<IEnumerable<PointOfInterestDto>> GetPointOfInterest(
+            int userId, int pointOfInterestId)
+        {
+            var user = UsersDataStore.Current.Users.FirstOrDefault(u => u.Id == userId);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            //find point of interest
+            var pointOfInterest = user.PointsOfInterest
+                .FirstOrDefault(p => p.Id == pointOfInterestId);
+
+            if (pointOfInterest == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(pointOfInterest);
+        }
+
+    }
+}
---
+++ ReturnAPI/UserInfo/UserInfo.API/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserInfo.API.Models;
+
+namespace UserInfo.API.Controllers
+{
+    //ControllerBase : Contains basic functionality controllers need like access to model state,
+    //the current user and common methods for returning responses.
+    //Controller : Contains addition methods when returning views which is not need when building an API
+
+    [ApiController]
+    [Route("api/users")]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<UserDto>> GetUsers()
+        { 
+            return Ok (UsersDataStore.Current.Users);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<UserDto> GetCity(int id)
+        {
+            //find a users
+            var userToReturn = UsersDataStore.Current.Users
+                .FirstOrDefault(c => c.Id == id);
+
+            if (userToReturn == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(userToReturn);
+        }
+    }
+}
